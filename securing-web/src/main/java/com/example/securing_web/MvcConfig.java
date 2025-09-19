009ef2b90459b543fd1823c5cc27b5b419b2f56d
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2025-09-18 21:05:19 -0400

    finished securing web tutorial!

---
+++ securing-web/src/main/java/com/example/securing_web/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.example.securing_web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
